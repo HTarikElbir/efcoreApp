9d10311d4f76a14607090f7824b45a662286e78a
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-19 05:57:26 +0300

    Add InitialCreate Migration

--- efcoreApp/Data/DataDbContext.cs
+++ efcoreApp/Data/DataDbContext.cs
@@ -5,6 +5,9 @@ namespace efcoreApp.Data
 {
     public class DataDbContext : DbContext
     {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+        }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<CourseRegistration> CourseRegistrations { get; set; }
--- efcoreApp/Program.cs
+++ efcoreApp/Program.cs
@@ -1,9 +1,16 @@
+using efcoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<DataDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
