4e2953874ac0fb300c159149f4389985951b4b88
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-19 22:00:38 +0300

    feat: Add StudentController and CreateStudent function

---
+++ efcoreApp/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using efcoreApp.Data;
+using efcoreApp.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace efcoreApp.Controllers
+{
+    public class StudentController(DataDbContext context) : Controller
+    {
+        private readonly DataDbContext _context = context;
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (student == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+           
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
