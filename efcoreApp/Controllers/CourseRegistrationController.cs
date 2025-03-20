3b0ee6accea54d0c2ba84e11df85af1d1f8d0c74
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-20 22:18:04 +0300

    feat: Add student course registration page

---
+++ efcoreApp/Controllers/CourseRegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Controllers
+{
+    public class CourseRegistrationController(DataDbContext context) : Controller
+    {
+        public readonly DataDbContext _context = context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Students = new SelectList(await _context.Students.ToListAsync(),"Id","Name");
+            ViewBag.Courses = new SelectList(await _context.Courses.ToListAsync(), "Id", "Name");
+            return View();
+        }
+    }
+}
