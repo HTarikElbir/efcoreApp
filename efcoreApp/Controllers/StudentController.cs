b6f1869a23f71a04ed3cfd673e3a76b39b83280b
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-20 17:54:28 +0300

    feat: Add update function to edit page

--- efcoreApp/Controllers/StudentController.cs
+++ efcoreApp/Controllers/StudentController.cs
@@ -33,6 +33,7 @@ namespace efcoreApp.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -46,6 +47,39 @@ namespace efcoreApp.Controllers
                 return NotFound();
             }
             return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public async Task<IActionResult> Edit(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();  
+                }
+                catch (DbUpdateConcurrencyException) 
+                { 
+                    if(!_context.Students.Any(s => s.Id == student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(student);
+
         }
     }
 }
